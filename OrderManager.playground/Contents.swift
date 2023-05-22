fb47695d95f5424b333aee78263560da76df07d4
AD <silentflute.ad@gmail.com>
2023-05-22 11:52:54 +0100

    do a simpel version for now

--- OrderManager.playground/Contents.swift
+++ OrderManager.playground/Contents.swift
@@ -1,54 +1,13 @@
 import Foundation
 
-enum OrderType: Int {
-    case initialOrder
-    case order
-    case sale
-}
-
-extension OrderType : CustomDebugStringConvertible {
-    var debugDescription: String {
-        switch self {
-        case .initialOrder: return "initialOrder"
-        case .order: return "order"
-        case .sale: return "sale"
-        }
-    }
-}
-
-
-
-struct Order {
-    let type: OrderType
-    let value: Int
-    
-    func changeType(to: OrderType, value: Int) -> Self {
-        switch to {
-        case .initialOrder:
-            return .init(type: to, value: value)
-        case .order:
-            return .init(type: to, value: value)
-        case .sale:
-            return .init(type: to, value: value)
-        }
-    }
-}
-
-
-
 class Locomotive {
-    var orders: [Order]
+    var initialOrder: Int?
+    var orders: [Int]
+    var sales: [Int]
     
-    init(orders: [Order] = [Order]() ) {
+    init(initialOrder: Int? = nil, orders:[Int], sales:[Int]) {
+        self.initialOrder = initialOrder
         self.orders = orders
+        self.sales = sales
     }
 }
-
-extension Locomotive : CustomDebugStringConvertible {
-    var debugDescription: String {
-        return ("orders: \(orders)")
-    }
-}
-
-let loco = Locomotive.init(orders: [Order.init(type: .initialOrder, value: 1)])
-print (loco.orders)
